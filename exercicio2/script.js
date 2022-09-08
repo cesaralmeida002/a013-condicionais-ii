cb3369252daf6fec8522262507aca436fd61382a
cesaralmeida002 <cesaralmeida002@gmail.com>
2022-09-08 16:25:07 -0300

    Exercícios 08/09/2022

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,4 +1,4 @@
-let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
+/*let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
 
 if(nacionalidade === "brasileira"){
     console.log("a pessoa é do Brasil!");
@@ -12,4 +12,29 @@ if(nacionalidade === "brasileira"){
     console.log("a pessoa é da Colômbia!");
 } else{
     console.log("nacionalidade não encontrada")
+}
+Recrie o exercício 3 da aula anterior (A012) de condicionais I, 
+utilizando switch-case.
+ No arquivo `script,js` existe uma versão do código já escrita, 
+mas você pode colar o exercício que você fez na última prática!*/
+
+let nacionalidade = prompt("Qual sua nacionalidade").toLowerCase()
+switch (nacionalidade) {
+    case "brasileira":
+        console.log("Sua nacionalidade, é BRASILEIRA")
+        break
+    case "argentina":
+        console.log("Sua nacionalidade, é ARGENTINA")
+        break
+    case "uruguaia":
+        console.log("Sua nacionalidade, é URUGUAIA")
+        break
+    case "chilena":
+        console.log("Sua nacionalidade, é CHILENA")
+        break
+    case "colombiana":
+        console.log("Sua nacionalidade, é COLOMBIANA")
+        break            
+    default:
+        console.log("nacinalidade não encontrada!!")
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,21 @@
 //código a ser reescrito
 
+/*
 if(nome === "José"){
 	console.log("Oi, Zé!");
 } else {
 	console.log("Olá, "+nome);
 }
+*/
+let nome = prompt("Digite seu nome").toLowerCase()
+	console.log(nome === "jose" ? "Oi Zé" : " Oi," + nome)
 
+/*
 if(idade >= 18){
 	console.log("pode tirar carteira de motorista!");
 } else {
 	console.log("Ainda não pode tirar carteira de motorista");
 }
+*/
+let idade = Number(prompt("Digite sua idade"))
+	console.log(idade >= 18 ? "Pode tirar a habilitação" : "Não pode tirar a habilitação")
